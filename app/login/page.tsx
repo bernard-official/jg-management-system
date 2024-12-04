a52f1bdf6465106b01e269806f59ae432e9633c3
Bernard <bdnterful@gmail.com>
2024-12-04 11:15:52 +0000

    WIP

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 import { signIn, signUp } from "@/actions/actions";
 // import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 import { SubmitButton } from "./submit-button";
 
 // const Login = ({ searchParams }: { searchParams: { message: string } }) => {
+
 const Login = () => {
+  const [sign, setSign] = useState(false);
+  const toggleButton = () => {
+    setSign(!sign);
+  };
+
   return (
     <div className="flex min-h-screen justify-center items-center  ">
       {/* <LoginForm /> */}
@@ -14,9 +21,24 @@ const Login = () => {
       <Card className="p-8  w-[400px] md:w-1/4 .h-[60vh] border  flex flex-col justify-center">
         <form className="space-y-8">
           <div className="flex justify-center">
-
-          <span className="text-2xl font-bold"> JASGLYNN</span>
+            <span className="text-2xl font-bold"> JASGLYNN</span>
           </div>
+          {!sign ? (
+            <span className=" text-sm ml-8">
+              Don&apos;t have an account?{" "}
+              <span onClick={toggleButton} className="underline hover:cursor-pointer hover:-translate-y-2">
+                Sign Up
+              </span>
+            </span>
+          ) : (
+            <span className=" text-sm ml-8">
+              Already have an account?{" "}
+              <span onClick={toggleButton} className="underline  hover:cursor-pointer hover:-translate-y-2">
+                Sign In
+              </span>{" "}
+            </span>
+          )}
+
           <div>
             <Input placeholder="example@email.com" name="email" />
           </div>
@@ -30,20 +52,16 @@ const Login = () => {
           
           <Button formAction={signUp}>Sign up</Button> */}
 
-            <SubmitButton
-              formAction={signIn}
-              // className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-              pendingText="Signing In..."
-            >
-              Sign In
-            </SubmitButton>
-            <SubmitButton
-              formAction={signUp}
-              // className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-              pendingText="Signing Up..."
-            >
-              Sign Up
-            </SubmitButton>
+            {!sign ? (
+              <SubmitButton formAction={signIn} pendingText="Signing In...">
+                Sign In
+              </SubmitButton>
+            ) : (
+              <SubmitButton formAction={signUp} pendingText="Signing Up...">
+                Sign Up
+              </SubmitButton>
+            )}
+
             {/* {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
             {searchParams.message}
@@ -52,8 +70,11 @@ const Login = () => {
           </div>
         </form>
         <div>
-          <span className="font-thin underline text-sm">forgot Password?</span>
-          </div>
+          {!sign ?
+          <span className="font-thin underline text-sm">forgot Password?</span>:
+          ''
+          }
+        </div>
       </Card>
     </div>
   );
